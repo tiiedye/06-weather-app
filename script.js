ce64d94a87d0fbc67e0db3dab381ab390047d141
Julia Clements <tiiedye@gmail.com>
2020-05-04 11:47:33 -0600

    fixed issues with local storage pull

--- script.js
+++ script.js
@@ -1,7 +1,101 @@
-$(document).ready(); {
+var loadCities = []
 
+init();
 
-
-
-
+function renderCities() {
+    for (var i = 0; i < loadCities.length; i++) {
+    var newBtn = $("<button>");
+    var br = $("<br>");
+    newBtn.addClass("btn btn-light city");
+    newBtn.attr("data-name", loadCities[i]);
+    newBtn.text(loadCities[i]);
+    $("#newCities").append(newBtn);
+    $("#newCities").append(br);
+  }
 }
+
+function init() {
+  var storedCities = JSON.parse(localStorage.getItem("loadCities"));
+
+  if (storedCities !== null) {
+    loadCities = storedCities;
+  }
+  renderCities();
+}
+
+function storeCities() {
+  localStorage.setItem("loadCities", loadCities);
+}
+
+
+$(".searchBtn").on("click", function() {
+
+    var cityName = $(".cityNameInput").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=de86690dd9d7e0f7bd2fcb9973ca0788"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+            console.log(response);
+
+        $("#name").text(response.name);
+        console.log(response.name);
+
+        var temperature = ((JSON.parse(response.main.temp) - 273.15) * 1.80 + 32);
+
+        $("#temp").text("Temperature: " + temperature);
+        console.log(temperature);
+
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        console.log(response.main.humidity);
+
+        $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+        console.log(response.wind.speed);
+
+        var newBtn = $("<button>");
+        var br = $("<br>");
+        newBtn.addClass("btn btn-light city");
+        newBtn.attr("data-name", response.name);
+        newBtn.text(response.name);
+        $("#newCities").append(newBtn);
+        $("#newCities").append(br);
+
+        var addedCityName = $(newBtn).attr("data-name");
+        // addedCityName.split('"').join('');
+        loadCities.push(addedCityName);
+        localStorage.setItem("loadCities", JSON.stringify(loadCities));
+
+        // storeCities();
+        });
+});
+
+$(".city").on("click", function() {
+
+  var cityName = $(this).attr("data-name");
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=de86690dd9d7e0f7bd2fcb9973ca0788"
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+          console.log(response);
+
+      $("#name").text(response.name);
+      console.log(response.name);
+
+      var temperature = ((JSON.parse(response.main.temp) - 273.15) * 1.80 + 32);
+
+      $("#temp").text("Temperature: " + temperature);
+      console.log(temperature);
+
+      $("#humidity").text("Humidity: " + response.main.humidity + "%");
+      console.log(response.main.humidity);
+
+      $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+      console.log(response.wind.speed);
+
+      });
+});
